59284231bacd502e4a6e48657f018e86e454863b
youssef-lahdhiri <lahdhiriyoussef0@gmail.com>
2024-05-29 18:46:50 +0200

    last

--- src/app/converter/Main.tsx
+++ src/app/converter/Main.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 
 export default function Main() {
   // const value = useContext(RightContext);
-  console.log(`value is ${value}`)
+  // console.log(`value is ${value}`)
   return (
     <div className="bg-blue-200 absolute top-1/2 right-1/2  translate-x-1/2"> this is converted value {value}</div>
   );
